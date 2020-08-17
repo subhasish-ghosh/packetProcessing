34b85d5c68951adcbb8b30f8c0e4462166913310
sg1g18 <sg1g18@soton.ac.uk>
2020-08-17 12:49:19 +0100

    Added build for Clion

---
+++ cmake-build-debug/CMakeFiles/CodingTest.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/subhasish/Southampton_MSc_Data/careers/cdp/CodingTest/CDP_BatteryFactory.cpp" "/mnt/d/subhasish/Southampton_MSc_Data/careers/cdp/CodingTest/cmake-build-debug/CMakeFiles/CodingTest.dir/CDP_BatteryFactory.cpp.o"
+  "/mnt/d/subhasish/Southampton_MSc_Data/careers/cdp/CodingTest/CDP_BatteryLogger.cpp" "/mnt/d/subhasish/Southampton_MSc_Data/careers/cdp/CodingTest/cmake-build-debug/CMakeFiles/CodingTest.dir/CDP_BatteryLogger.cpp.o"
+  "/mnt/d/subhasish/Southampton_MSc_Data/careers/cdp/CodingTest/CDP_BatteryMonitor.cpp" "/mnt/d/subhasish/Southampton_MSc_Data/careers/cdp/CodingTest/cmake-build-debug/CMakeFiles/CodingTest.dir/CDP_BatteryMonitor.cpp.o"
+  "/mnt/d/subhasish/Southampton_MSc_Data/careers/cdp/CodingTest/CDP_BatteryPackets.cpp" "/mnt/d/subhasish/Southampton_MSc_Data/careers/cdp/CodingTest/cmake-build-debug/CMakeFiles/CodingTest.dir/CDP_BatteryPackets.cpp.o"
+  "/mnt/d/subhasish/Southampton_MSc_Data/careers/cdp/CodingTest/CDP_BatteryParser.cpp" "/mnt/d/subhasish/Southampton_MSc_Data/careers/cdp/CodingTest/cmake-build-debug/CMakeFiles/CodingTest.dir/CDP_BatteryParser.cpp.o"
+  "/mnt/d/subhasish/Southampton_MSc_Data/careers/cdp/CodingTest/CDP_BatteryPower.cpp" "/mnt/d/subhasish/Southampton_MSc_Data/careers/cdp/CodingTest/cmake-build-debug/CMakeFiles/CodingTest.dir/CDP_BatteryPower.cpp.o"
+  "/mnt/d/subhasish/Southampton_MSc_Data/careers/cdp/CodingTest/CDP_BatteryStatus.cpp" "/mnt/d/subhasish/Southampton_MSc_Data/careers/cdp/CodingTest/cmake-build-debug/CMakeFiles/CodingTest.dir/CDP_BatteryStatus.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CodingTest.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CodingTest.dir/CDP_BatteryFactory.cpp.o"
+  "CMakeFiles/CodingTest.dir/CDP_BatteryLogger.cpp.o"
+  "CMakeFiles/CodingTest.dir/CDP_BatteryMonitor.cpp.o"
+  "CMakeFiles/CodingTest.dir/CDP_BatteryPackets.cpp.o"
+  "CMakeFiles/CodingTest.dir/CDP_BatteryParser.cpp.o"
+  "CMakeFiles/CodingTest.dir/CDP_BatteryPower.cpp.o"
+  "CMakeFiles/CodingTest.dir/CDP_BatteryStatus.cpp.o"
+  "CodingTest"
+  "CodingTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CodingTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
